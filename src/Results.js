e0a6f07c796f60e695a3341f2739f3843fc0d6a3
Adam Leskis <leskis@gmail.com>
2023-08-23 20:50:12 +0100

    start calculating p50 & p90 SLIs

--- src/Results.js
+++ src/Results.js
@@ -1,21 +1,56 @@
-import { VictoryBar, VictoryChart } from "victory";
-const style = {
-    data: {
-        fill: "tomato",
-    },
+// shameless steal from SO
+const median = (values) => {
+    values.sort((a, b) => {
+        return a - b;
+    });
+
+    const half = Math.floor(values.length / 2);
+
+    if (values.length % 2) return values[half];
+
+    return (values[half - 1] + values[half]) / 2.0;
 };
+
+const calcQuartile = (arr, q) => {
+    const a = arr.slice();
+    // Turn q into a decimal (e.g. 95 becomes 0.95)
+    q = q / 100;
+
+    // Sort the array into ascending order
+    const data = sortArr(a);
+
+    // Work out the position in the array of the percentile point
+    const p = (data.length - 1) * q;
+    const b = Math.floor(p);
+
+    // Work out what we rounded off (if anything)
+    const remainder = p - b;
+
+    // See whether that data exists directly
+    if (data[b + 1] !== undefined) {
+        return (
+            parseFloat(data[b]) +
+            remainder * (parseFloat(data[b + 1]) - parseFloat(data[b]))
+        );
+    } else {
+        return parseFloat(data[b]);
+    }
+};
+
+const sortArr = (arr) => {
+    var ary = arr.slice();
+    ary.sort((a, b) => {
+        return parseFloat(a) - parseFloat(b);
+    });
+    return ary;
+};
+
+const convertToSeconds = (milliseconds) => (milliseconds / 1000).toFixed(2);
+
 export default function Results({ data }) {
-    // because we need a deep copy of the array of objects
-    let tempData = JSON.parse(JSON.stringify(data));
-    // just in case somebody navigates here first without recording any preferences
-    const reduced =
-        data.length !== 0
-            ? tempData.reduce((sum, perception) => {
-                  sum[perception.fastEnough] =
-                      (sum[perception.fastEnough] || 0) + 1;
-                  return sum;
-              }, {})
-            : { true: 0, false: 0 };
+    const loadTimeData = data.map(({ delay }) => delay);
+    const p50 = convertToSeconds(median(loadTimeData));
+    const p90 = convertToSeconds(calcQuartile(loadTimeData, 90));
 
     return (
         <div>
@@ -24,19 +59,10 @@ export default function Results({ data }) {
                 satisfied with
             </h3>
             {data.length !== 0 ? (
-                <VictoryChart
-                    height={400}
-                    width={400}
-                    domainPadding={{ x: 50, y: [0, 20] }}
-                >
-                    <VictoryBar
-                        style={style}
-                        data={[
-                            { x: "too slow", y: reduced.false || 0 },
-                            { x: "fast enough", y: reduced.true || 0 },
-                        ]}
-                    />
-                </VictoryChart>
+                <div>
+                    <p>your p50 is {p50} seconds</p>
+                    <p>your p90 is {p90} seconds</p>
+                </div>
             ) : (
                 <p>no data yet!</p>
             )}
