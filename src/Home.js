5fe95443afe843f2f7d785e1205e05e718da46b0
Adam Leskis <leskis@gmail.com>
2023-08-25 22:47:59 +0100

    Update Home.js
    Remove mention of deleted page

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,12 @@
 export default function Home() {
     return (
         <div>
-            <div>This app has four sections:</div>
+            <div>This app has three sections:</div>
             <ol>
                 <li>this explanation page</li>
                 <li>
                     the main "home page" where you react to the page loading
                     speed
-                </li>
-                <li>
-                    the search page where you hit search and react to the search
-                    results speed
                 </li>
                 <li>
                     the results page where you can see what SLOs you would be
