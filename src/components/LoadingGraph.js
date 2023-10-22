97a58051294f3d49e52527ce7758de8ab2835f88
Adam Leskis <leskis@gmail.com>
2023-10-22 17:31:19 +0100

    restyle graph and buttons

--- src/components/LoadingGraph.js
+++ src/components/LoadingGraph.js
@@ -1,27 +1,32 @@
 import { Chart } from "react-google-charts";
 
-export const defaultData = [
-    ["Observations", "Load times in milliseconds", "lenient SLO", "strict SLO"],
-];
+export const defaultData = [["x", "load time", "lenient SLO", "strict SLO"]];
 
 export const options = {
-    chart: {
-        title: "Potential SLOs",
+    title: "Potential SLOs",
+    colors: ["blue", "orange", "red"],
+    hAxis: {
+        title: "observations",
     },
+    vAxis: {
+        title: "load times in milliseconds",
+    },
+    legend: { position: "bottom" },
 };
 
 const LoadingGraph = ({ averageUnhappy, data, slowestUnhappy }) => {
     // hack it!
     let modifiedData = [];
+    // we need to add the values for the computed SLOs into each array element of the 2D array
     data.forEach(([index, value]) => {
         modifiedData.push([index, value, averageUnhappy, slowestUnhappy]);
     });
 
     return (
         <Chart
-            chartType="Line"
+            chartType="LineChart"
             width="100%"
-            height="400px"
+            height="600px"
             data={defaultData.concat(modifiedData)}
             options={options}
         />
