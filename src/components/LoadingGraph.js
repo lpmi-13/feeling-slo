a0f98c7b456a32c13ca74e128091319b1f69fd2e
Adam Leskis <leskis@gmail.com>
2023-10-24 21:07:44 +0100

    complete the legend shortening

--- src/components/LoadingGraph.js
+++ src/components/LoadingGraph.js
@@ -1,6 +1,6 @@
 import { Chart } from "react-google-charts";
 
-export const defaultData = [["x", "load time", "mild", "strict"]];
+export const defaultData = [["x", "time", "mild", "strict"]];
 
 export const options = {
     title: "Potential SLOs",
